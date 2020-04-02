95b1296992d77a3f0f7b4cfb8d859ed446515687
Igor Eulálio <igoreulalio.ie@gmail.com>
2020-04-02 19:40:28 -0300

    incluindo classe de configuracao.

--- src/main/java/com/epass/backgroundrop/adapters/UserController.java
+++ src/main/java/com/epass/backgroundrop/adapters/UserController.java
@@ -1,10 +1,16 @@
 package com.epass.backgroundrop.adapters;
 
+
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class UsuarioController {
+@RequestMapping("/users")
+public class UserController {
+
+
 
     @GetMapping("/")
     public String index(){
